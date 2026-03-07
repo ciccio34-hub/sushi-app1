23361ceb3e40135b2e5926051e911397e7ff0af2
ciccio34-hub <corrado.delfino@libero.it>
2026-03-07 09:08:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+let cart = [];
+let total = 0;
+
+function addToCart(name, price){
+
+cart.push({name, price});
+total += price;
+
+updateCart();
+
+}
+
+function updateCart(){
+
+let cartList = document.getElementById("cart");
+cartList.innerHTML = "";
+
+cart.forEach(item => {
+
+let li = document.createElement("li");
+li.textContent = item.name + " - €" + item.price;
+cartList.appendChild(li);
+
+});
+
+document.getElementById("total").textContent = "Totale: €" + total;
+
+}
+
+function sendOrder(){
+
+let orderText = "Ordine Sushi:%0A";
+
+cart.forEach(item=>{
+orderText += item.name + " €" + item.price + "%0A";
+});
+
+orderText += "Totale €" + total;
+
+let phone = "391234567890"; 
+
+let url = "https://wa.me/"+phone+"?text="+orderText;
+
+window.open(url);
+
+}
+
 
 
 
